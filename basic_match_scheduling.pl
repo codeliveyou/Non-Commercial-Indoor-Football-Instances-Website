7dbc53207198526b388ba8943fc4995030aecf97
codeliveyou <codetankstn@gmail.com>
2024-06-03 03:01:29 -0600

    deleted old code

--- basic_match_scheduling.pl
+++ basic_match_scheduling.pl
@@ -4,6 +4,8 @@
 % Load the input file
 :- consult('PrologFacts/Input1.pl').
 
+
+
 not_same_team(T1, T2) :- T1 #\= T2.
 
 % Constraint 2 & 3: Home team availability and away team unavailability are respected
